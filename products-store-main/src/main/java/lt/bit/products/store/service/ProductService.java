e84085ef7f9d4472acffe3a67a28e08ecf5f3ff4
PauliusBis <97383171+PauliusBis@users.noreply.github.com>
2022-06-16 22:04:24 +0300

    Add product delete save

--- products-store-main/src/main/java/lt/bit/products/store/service/ProductService.java
+++ products-store-main/src/main/java/lt/bit/products/store/service/ProductService.java
@@ -3,8 +3,10 @@ package lt.bit.products.store.service;
 import java.util.List;
 import lt.bit.products.store.model.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ProductService {
 
   private final ProductRepository repository;
@@ -22,6 +24,11 @@ public class ProductService {
   }
 
   public void deleteProduct(Integer id) {
+    repository.deleteStoreItems(id);
     repository.deleteById(id);
+  }
+
+  public Product saveProduct(Product product) {
+    return repository.save(product);
   }
 }
